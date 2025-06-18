388183fa8c193691a40b60b1ecaaa9a5f1df8026
Chris Messer <cmesser@manh.com>
2025-06-18 18:23:54 -0400

    cleaning up docker

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Default to running the Streamlit app if no command is provided
-if [ $# -eq 0 ]; then
-    exec streamlit run app.py --server.port=8080 --server.address=0.0.0.0 --server.headless=true --browser.gatherUsageStats=false
-else
-    exec "$@"
-fi
